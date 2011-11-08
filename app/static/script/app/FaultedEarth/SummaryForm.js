f846f4a118b71d1866c6981809e122d6f1af129d
Ben Wyss <bmwyss@gmail.com>
2011-11-08 11:28:42 +0100

    clean up some more

--- app/static/script/app/FaultedEarth/SummaryForm.js
+++ app/static/script/app/FaultedEarth/SummaryForm.js
@@ -43,12 +43,12 @@ FaultedEarth.SummaryForm = Ext.extend(gxp.plugins.Tool, {
                 if (!e.feature.fid) {
                     return;
                 }
-                if (featureManager.layerRecord.get("name") == "geonode:fault_summary") {
+                if (featureManager.layerRecord.get("name") == "geonode:fault_section_view") {
                     this.target.summaryId = e.feature.fid;
                 }
             },
             "featureunselected": function(e) {
-                if (this.active && featureManager.layerRecord.get("name") == "geonode:fault_summary") {
+                if (this.active && featureManager.layerRecord.get("name") == "geonode:fault_section_view") {
                     this.target.summaryId = null;
                 }
             },
@@ -122,7 +122,7 @@ FaultedEarth.SummaryForm = Ext.extend(gxp.plugins.Tool, {
             featureManager.setLayer();
             if (!this.layerRecord) {
                 this.target.createLayerRecord({
-                    name: "geonode:fault_summary",
+                    name: "geonode:fault_section_view",
                     source: "local"
                 }, function(record) {
                     this.layerRecord = record;
